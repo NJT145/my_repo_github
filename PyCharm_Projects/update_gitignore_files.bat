696f5e2601cc5610bc722f3cfad30dfb6b1787b1
Nejat Günaydın <njt145@gmail.com>
2019-11-08 14:32:23 +0300

    ...

--- PyCharm_Projects/update_gitignore_files.bat
+++ PyCharm_Projects/update_gitignore_files.bat
@@ -4,12 +4,14 @@
 
 @cmd /C  "gi python > %myRoot%\py2\.gitignore"
 @cmd /C  "echo Pipfile.lock >> %myRoot%\py2\.gitignore"
+@cmd /C  "echo .venv >> %myRoot%\py2\.gitignore"
 
 @cmd /C  "gi python > %myRoot%\py3\.gitignore"
 @cmd /C  "echo Pipfile.lock >> %myRoot%\py3\.gitignore"
+@cmd /C  "echo .venv >> %myRoot%\py3\.gitignore"
 
 @cmd /C  "gi python > %myRoot%\ResizeNSaveImageToBTM\.gitignore"
 @cmd /C  "echo Pipfile.lock >> %myRoot%\ResizeNSaveImageToBTM\.gitignore"
-@cmd /C  "echo venv >> %myRoot%\ResizeNSaveImageToBTM\.gitignore"
+@cmd /C  "echo .venv >> %myRoot%\ResizeNSaveImageToBTM\.gitignore"
 
 @endlocal
